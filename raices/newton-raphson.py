91d6ef390a03708e0474c1c117a939451338973b
MarioQuiroga <marioquiro17@gmail.com>
2018-04-29 20:05:08 -0300

    Update newton-raphson.py

--- raices/newton-raphson.py
+++ raices/newton-raphson.py
@@ -7,7 +7,7 @@ from sympy.parsing.mathematica import mathematica
 from sympy.parsing.maxima import parse_maxima
 from sympy import *
 
-#	RECIBE UNA FUNCIÓN EN FORMATO STRING, x ES UN VALOR SUPUESTAMENTE CERCANO A LA RAIZ		
+#	RECIBE UN FUNCION EN FORMATO STRING, x ES UN VALOR SUPUESTAMENTE CERCANO A LA RAIZ		
 def newtonRaphson(exp, x, error):	
 	#PARSEO LA FUNCION
 	f = mathematica(exp)
@@ -16,40 +16,40 @@ def newtonRaphson(exp, x, error):
 	#CALCULO LA DERIVADA
 	df = diff(f)	
 	print ('f\'(x) = '+str(df))
-	
-	#MIENTRAS Fx SEA MAYOR QUE EL ERROR
 	while(abs(f.subs('x',x))>error):
 		xAnt = x
 		
 		#IMPRIMO X y F(X) EN EL PUNTO 
-		print ('f(x) = '+str(f.subs('x',xAnt)))
-		print ('x = '+str(xAnt))
+		print ('f(x) = '+str(float(f.subs('x',xAnt))))
+		print ('x = '+str(float(xAnt)))
 		
 		# EL X SIGUIENTE ES: X(n+1) = Xn - (f(Xn)/f'(Xn))
 		x = x - (f.subs('x',x)/df.subs('x',x))		
 		
 		# DEFINO LA RECTA TANGENTE 
-		rect = str(df.subs('x',xAnt))+'*x+'+str(f.subs('x',xAnt)-df.subs('x',xAnt)*xAnt)		
+		rect = str(float(df.subs('x',xAnt)))+'*x+'+str(float(f.subs('x',xAnt)-df.subs('x',xAnt)*xAnt))		
 		frect = mathematica(rect)
 		
 
 		print ('Recta: ', frect)
-		print ('X(n+1) = ', x) 
-		print ('f(X(n+1)) = ',f.subs('x',x)) 
+		print ('X(n+1) = ', float(x)) 
+		print ('f(X(n+1)) = ',float(f.subs('x',x))) 
 		
 		# 
 		if(f.subs('x',x)<=error):
 			print('El algoritmo ha convergido. f(X(n+1))<=error')			
 		#GRAFICO DE LA FUNCION Y LA RECTA TANGENTE		
-		print ('Cierre La gráfica de la función para continuar')
 		plot(f, frect)
 		
 
 #############################################################################################
 # 	MAIN		
 
-Fx = 'x**3'   # PASO LA FUNCION EN FORMATO STRING
-puntoDeArranque = 5
-error = 0.1
+if __name__ == "__main__":
+    #Fx = 'sin[x]'   # PASO LA FUNCION EN FORMATO STRING
+    Fx = '3x**3+5x**2'
+    puntoDeArranque = 5
+    error = 0.1
 
-newtonRaphson(Fx,puntoDeArranque,error)			
+    newtonRaphson(Fx,puntoDeArranque,error)			
+		
